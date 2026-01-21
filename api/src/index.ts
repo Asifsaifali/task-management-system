59a4b1f354eb9c9d3c181f55d6efd8b2050c33b8
Asif Saif Ali <asif263848@gmail.com>
2026-01-21 19:10:13 +0530

    push remaning files

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,14 +1,14 @@
 import express from 'express';
 import cors from 'cors';
-
-
+import authRoutes from './auth/auth.routes'
 const app = express();
 const PORT = 4000;
 
 
 const startServer = async () => {
     try {
-
+        app.use(express.json());
+        app.use('/v1/auth',authRoutes);
         app.use(cors({
             origin: '*',
             methods: ['GET', 'POST', 'PUT', 'DELETE'],
